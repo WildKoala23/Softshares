e2e4c2238eff405d746f706e9525e77ec6b868f9
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-08-02 22:06:51 +0100

    firebase started
    Had to run flutter clean to build from anew.
    Also add to add min version to android sdk

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:softshares/classes/ClasseAPI.dart';
-import 'package:softshares/classes/user.dart';
+import 'package:softshares/classes/user.dart' as u;
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:crypto/crypto.dart';
 import 'package:softshares/classes/db.dart';
 import 'package:softshares/classes/areaClass.dart';
 
+//firebase
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
 class AuthProvider with ChangeNotifier {
   final FlutterSecureStorage storage = FlutterSecureStorage();
   final box = GetStorage();
-  User? _user;
+  u.User? _user;
   bool _isLoggedIn = false;
   List<AreaClass> _areas = [];
   Map<String, int> _cities = {};
   SQLHelper bd = SQLHelper.instance;
   API api = API();
-  User? get user => _user;
+  u.User? get user => _user;
   bool get isLoggedIn => _isLoggedIn;
   List<AreaClass> get areas => _areas;
   Map<String, int> get cities => _cities;
@@ -40,7 +46,7 @@ class AuthProvider with ChangeNotifier {
     _user = user;
 
     // If checkbox is selected
-    if(keepSign){
+    if (keepSign) {
       await bd.insertUser(user!.firstname, user.id, user.lastName, user.email);
     }
 
@@ -84,5 +90,31 @@ class AuthProvider with ChangeNotifier {
     SQLHelper db = SQLHelper.instance;
     _areas = await db.getAreas();
     _cities = await db.getCities();
+  }
+
+  Future<firebase.User?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final firebase.AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    final UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    return userCredential.user;
+  }
+
+  Future<User?> signInWithFacebook() async {
+    final LoginResult result = await FacebookAuth.instance.login();
+
+    final AuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(result.accessToken!.token);
+
+    final UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithCredential(facebookAuthCredential);
+    return userCredential.user;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,23 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_auth
+import firebase_core
+import firebase_messaging
 import flutter_secure_storage_macos
-import package_info_plus
+import google_sign_in_ios
 import path_provider_foundation
-import sentry_flutter
+import share_plus
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
